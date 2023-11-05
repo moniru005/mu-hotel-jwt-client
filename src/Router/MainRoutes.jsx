45ba95095e9d58bf662be458dfbb22861ac511fc
Monir <moniru005@gmail.com>
2023-11-05 11:02:10 +0600

    Router provider and Routes setup

---
+++ src/Router/MainRoutes.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
+import Home from "../Pages/Home";
+
+const MainRoutes = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        errorElement:<ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+
+export default MainRoutes;
